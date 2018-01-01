c5e79d74c5fb1630d5884f43bb9e19fc64e6341a
Saso Sabotin <sasos90@gmail.com>
2018-01-01 12:37:37 +0100

    Copy command to clipboard

--- src/components/Cheat.js
+++ src/components/Cheat.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Cheat.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class Cheat extends Component {
   render() {
     return (
       <div className="Cheat">
         <div className="desc">{this.props.desc}</div>
-        <div className="command"><span>$ </span><span className="cmd">{this.props.command}</span></div>
+        <CopyToClipboard text={this.props.command}>
+          <div className="command"><span>$ </span><span className="cmd">{this.props.command}</span></div>
+        </CopyToClipboard>
       </div>
     );
   }
